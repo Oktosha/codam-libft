fe2af682a0f4856d96370f4b4a58a66ee4478c5b
Daria Kolodzey <dkolodze@student.codam.nl>
2022-10-10 18:08:18 +0200

    Add bonus tests template

--- task/ft_lstnew.c
+++ task/ft_lstnew.c
@@ -1,26 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   libft.h                                            :+:    :+:            */
+/*   ft_lstnew.c                                        :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: dkolodze <dkolodze@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/10/03 17:48:45 by dkolodze      #+#    #+#                 */
-/*   Updated: 2022/10/10 17:25:35 by dkolodze      ########   odam.nl         */
+/*   Created: 2022/10/10 17:26:27 by dkolodze      #+#    #+#                 */
+/*   Updated: 2022/10/10 17:40:28 by dkolodze      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include <stdlib.h>
+#include "libft.h"
 
-int	ft_isalpha(int c);
-
-typedef struct s_list
+t_list	*ft_lstnew(void *content)
 {
-	void			*content;
-	struct s_list	*next;
-}	t_list;
+	t_list *node;
 
-t_list	*ft_lstnew(void *content);
-
-#endif
+	node = malloc(sizeof(t_list));
+	if (node == NULL)
+	{
+		return NULL;
+	}
+	node->content = content;
+	node->next = NULL;
+	return node;
+}
--- tests/bonus.c
+++ tests/bonus.c
@@ -1,35 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
+/*   bonus.c                                            :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: dkolodze <dkolodze@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/10/06 15:47:11 by dkolodze      #+#    #+#                 */
-/*   Updated: 2022/10/10 18:02:11 by dkolodze      ########   odam.nl         */
+/*   Created: 2022/10/10 17:48:49 by dkolodze      #+#    #+#                 */
+/*   Updated: 2022/10/10 18:01:56 by dkolodze      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 #include "greatest.h"
 
-TEST test_ft_isalpha(void)
+TEST test_ft_lstnew(void)
 {
-	ASSERT(ft_isalpha('a'));
-	ASSERT(ft_isalpha('A'));
-	ASSERT(ft_isalpha('z'));
-	ASSERT(ft_isalpha('Z'));
-	ASSERT(ft_isalpha('b'));
-	ASSERT(ft_isalpha('B'));
-	ASSERT_FALSE(ft_isalpha('0'));
-	ASSERT_FALSE(ft_isalpha('\n'));
-	ASSERT_FALSE(ft_isalpha(' '));
+	char ch = 'a';
+	t_list *node = ft_lstnew(&ch);
+	ASSERT_EQ(node->next, NULL);
+	ASSERT_EQ(*((char *)(node->content)), 'a');
 	PASS();
 }
 
-SUITE(part1)
+SUITE(bonus)
 {
-	RUN_TEST(test_ft_isalpha);
+	RUN_TEST(test_ft_lstnew);
 }
 
 GREATEST_MAIN_DEFS();
@@ -37,6 +32,6 @@ GREATEST_MAIN_DEFS();
 int main(int argc, char **argv)
 {
     GREATEST_MAIN_BEGIN();
-	RUN_SUITE(part1);
+	RUN_SUITE(bonus);
     GREATEST_MAIN_END();
 }
